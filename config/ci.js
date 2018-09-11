c63682e85004e863f3ceb1f58b21a79c7c0ef60d
Man Yiu <wongmanyiu@gmail.com>
2018-09-12 00:47:42 +0800

    travis config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "IAxU0fzOxKg9X9Avt8m0oRbEKvZ5EfVPPM6nZf2iq7t0yU3jp3AR4ObcXMqqb5Un",
+  redisUrl: "redis://127.0.0.1:6379"
+};
--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb://devuser:WHqY9pdTPs8Ch2I6fzSooovsFZQgstrL@ds018568.mlab.com:18568/blog_dev",
+  cookieKey: "IAxU0fzOxKg9X9Avt8m0oRbEKvZ5EfVPPM6nZf2iq7t0yU3jp3AR4ObcXMqqb5Un",
+  redisUrl: "redis://127.0.0.1:6379"
 };
